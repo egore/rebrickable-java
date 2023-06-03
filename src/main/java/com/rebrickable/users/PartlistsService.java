47b9b8ab89eaa3119e8cc91e40a3aebf4d34be5e
Christoph Brill <opensource@christophbrill.de>
2023-06-03 23:15:44 +0200

    feat: Add create, delete, update and replace to PartlistsService

--- src/main/java/com/rebrickable/users/PartlistsService.java
+++ src/main/java/com/rebrickable/users/PartlistsService.java
@@ -5,7 +5,9 @@ import com.rebrickable.users.model.Partlist;
 import com.rebrickable.users.responses.PartlistsResponse;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PartlistsService extends AbstractUserService {
 
@@ -23,6 +25,33 @@ public class PartlistsService extends AbstractUserService {
 
     public List<Partlist> parts(int listId) throws IOException {
         return getPaged("/users/{user_token}/partlists/{list_id}/parts/".replace("{user_token}", userToken).replace("{list_id}", Integer.toString(listId)), PartlistsResponse.class);
+    }
+
+    public Partlist create(Partlist partlist) throws IOException {
+        Map<String, String> data = toMap(partlist);
+        return post("/users/{user_token}/partlists/".replace("{user_token}", userToken), Partlist.class, data);
+    }
+
+    public void delete(int listId) throws IOException {
+        delete("/users/{user_token}/partlists/{list_id}/parts/".replace("{user_token}", userToken).replace("{list_id}", Integer.toString(listId)));
+    }
+
+    public Partlist update(Partlist partlist) throws IOException {
+        Map<String, String> data = toMap(partlist);
+        return put("/users/{user_token}/partlists/{list_id}/parts/".replace("{user_token}", userToken).replace("{list_id}", Integer.toString(partlist.id)), Partlist.class, data);
+    }
+
+    public Partlist replace(Partlist partlist) throws IOException {
+        Map<String, String> data = toMap(partlist);
+        return put("/users/{user_token}/partlists/{list_id}/parts/".replace("{user_token}", userToken).replace("{list_id}", Integer.toString(partlist.id)), Partlist.class, data);
+    }
+
+    private static Map<String, String> toMap(Partlist partlist) {
+        Map<String, String> data = new HashMap<>();
+        data.put("is_buildable", Boolean.toString(partlist.buildable));
+        data.put("name", partlist.name);
+        data.put("num_parts", Integer.toString(partlist.numParts));
+        return data;
     }
 
 }
